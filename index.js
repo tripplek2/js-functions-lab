c4151ea3e17ddf3097c5efce5eada5d0f517d1ab
kelvin <kibetkelvin222@gmail.com>
2026-03-24 17:33:15 +0300

    files updated

--- index.js
+++ index.js
@@ -1,7 +1,30 @@
-
-
-
-
-
 // This is required for the test to function properly  
 module.exports = { calculateTax, convertToUpperCase, findMaximum, isPalindrome, calculateDiscountedPrice };
+
+// Function 1: calculateTax
+function calculateTax(amount) {
+    return amount * 0.10;
+}
+
+// Function 2: connvertToUpperCase
+function convertToUpperCase(text) {
+    return text.toUpperCase();
+}
+
+// Function 3: findMaximum
+function findMaximum(num1, num2) {
+    return num1 > num2 ? num1 : num2;
+}
+
+// Function 4: isPalindrome
+function isPalindrome(word) {
+    let reversed = word.split("").reverse().join("");
+    return word === reversed;
+}
+
+// Function 5: calculateDiscountedPrice
+function calculateDiscountedPrice(originalPrice,discountPercentage) {
+    if (discountPercentage <= 0) return originalPrice;
+    const discountAmount = originalPrice * (discountPercentage / 100);
+    return originalPrice - discountAmount;
+}
